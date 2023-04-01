09e132f317c97fa9edd9db9132332e6d6474dd9a
IMS212 <ims@irisshaders.dev>
2023-04-01 14:49:40 -0700

    It works

--- src/main/java/net/coderbot/batchedentityrendering/mixin/CompositeStateAccessor.java
+++ src/main/java/net/coderbot/batchedentityrendering/mixin/CompositeStateAccessor.java
@@ -9,4 +9,7 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 public interface CompositeStateAccessor {
 	@Accessor("transparencyState")
 	RenderStateShard.TransparencyStateShard getTransparency();
+
+	@Accessor("outputState")
+    RenderStateShard.OutputStateShard getOutput();
 }
